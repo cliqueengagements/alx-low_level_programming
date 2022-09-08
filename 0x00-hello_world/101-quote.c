9dc8844ee1d22e101ce8942f5f8a043d8b3b3c57
cliqueengagements <e037403.olawunmi@dlc.ui.edu.ng>
2022-09-08 11:55:09 -0700

    quote file edited

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
-/**
- * main - main block
- * Return: 1
- */
-int main(void)
-{
-	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
-	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	      s);
+/**
+ * main -Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+
+{
+
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
+
+	write(2, str1, 59);
+
 	return (1);
+
 }
