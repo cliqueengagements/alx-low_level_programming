53434f75f8a84a1c42d1958ec51c38245ad77048
cliqueengagements <e037403.olawunmi@dlc.ui.edu.ng>
2022-09-15 06:38:49 -0700

    main

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -10,13 +10,12 @@ int main(void)
 {
 	int a, b, c;
 	int largest;
-
 	a = 972;
 	b = -98;
 	c = 0;
-
 	largest = largest_number(a, b, c);
 
 	printf("%d is the largest number\n", largest);
+
 	return (0);
 }
