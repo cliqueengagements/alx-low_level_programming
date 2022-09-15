783a17d131fbc49efe5308a270e8f35900772530
Eniola Samuel Olawunmi <83251815+cliqueengagements@users.noreply.github.com>
2022-09-15 14:26:05 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,15 +13,15 @@ int largest_number(int a, int b, int c)
 
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > c && b > a)
+	else if (b >= c && b >= a)
 	{
 		largest = b;
 	}
-	else if (c > b)
+	else if (c >= b)
 	{
 		largest = c;
 	}
