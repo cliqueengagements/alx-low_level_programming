29974e4068b852947ce5972cdc9f941119b82f52
cliqueengagements <e037403.olawunmi@dlc.ui.edu.ng>
2022-09-14 17:28:53 -0700

    editted main.h

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,11 +11,13 @@ int main(void)
 	int a, b, c;
 	int largest;
 
-	a = -972;
+	a = 972;
 	b = -98;
 	c = 0;
 
 	largest = largest_number(a, b, c);
+	
 	printf("%d is the largest number\n", largest);
+
 	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 
 void positive_or_negative(int n);
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
