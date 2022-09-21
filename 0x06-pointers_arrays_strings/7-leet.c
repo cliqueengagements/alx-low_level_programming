74bbbc8d3a06a29c3663f15300f8cd231f316ef8
cliqueengagements <e037403.olawunmi@dlc.ui.edu.ng>
2022-09-21 14:15:09 -0700

    no

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,15 +12,15 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		while (s[i] == 'a' || s[i] == 'A')
 			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
+		while (s[i] == 'e' || s[i] == 'E')
 			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
+		while (s[i] == 'o' || s[i] == 'O')
 			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
+		while (s[i] == 't' || s[i] == 'T')
 			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
+		while (s[i] == 'l' || s[i] == 'L')
 			s[i] = '1';
 	}
 
