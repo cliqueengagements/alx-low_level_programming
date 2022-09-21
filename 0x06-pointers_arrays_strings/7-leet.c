a1bbb5a7595d85a38a2c918b286c7428da7a564f
cliqueengagements <e037403.olawunmi@dlc.ui.edu.ng>
2022-09-21 14:34:19 -0700

    needed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,15 +12,15 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
+		if (s[i] == 'a' || s[i] == 'A')
 			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
+		if (s[i] == 'e' || s[i] == 'E')
 			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
+		if (s[i] == 'o' || s[i] == 'O')
 			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
+		if (s[i] == 't' || s[i] == 'T')
 			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
+		if (s[i] == 'l' || s[i] == 'L')
 			s[i] = '1';
 	}
 
