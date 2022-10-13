9159263db47318b0dd6ab1aa2b26d64746c4448e
Eniola Samuel Olawunmi <83251815+cliqueengagements@users.noreply.github.com>
2022-10-13 12:12:56 +0100

    Create variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
