340c5971318dd2ba0a3cd3a19faa9fc68e20adb8
Eniola Samuel Olawunmi <83251815+cliqueengagements@users.noreply.github.com>
2022-10-31 20:38:25 +0100

    Add files via upload

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: text
+ * Return: 1 - success, -1 error
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	unsigned int length;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		length = 0;
+		text_content = "";
+	}
+	else
+		length = _strlen(text_content);
+
+	bytes_written = write(fd, text_content, length);
+	if (bytes_written == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
